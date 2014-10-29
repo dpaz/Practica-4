1e023b6ad59693459edcded8c6e46488d409be78
David Paz Brea <dpaz1993@gmail.com>
2014-10-29 11:51:42 +0100

    Test de comprovacion de salida fuera del tablero

--- 09-enemies/spec/EnemySpec.js
+++ 09-enemies/spec/EnemySpec.js
@@ -72,7 +72,7 @@ describe("EnemySpec.js",function(){
     oldGame = Game;
     Game = {width: 320, height: 480};
     var enemies = {
-      basic: { x: 100, y: -50, sprite: 'enemy_purple', B: 100, C: 2 , E: 100 }
+      basic: { x: 100, y: -50, sprite: 'enemy_purple', B: 700, C: 2 , E: 100 }
     };
 
   });
@@ -86,6 +86,10 @@ describe("EnemySpec.js",function(){
 
     Game = oldGame;
     board = new GameBoard();
+
+    Game.initialize("game",sprites,function(){});  
+    Game.setBoard(1,board);
+
     enemy = new Enemy(enemies.basic);
     board.add(enemy);
     
@@ -96,7 +100,26 @@ describe("EnemySpec.js",function(){
   });
 
   it("Si salimos del tablero se elimina",function(){
+    Game =oldGame;
+
+    board = new GameBoard();
+    Game.initialize("game",sprites,function(){});  
+    Game.setBoard(1,board);
+
+    enemy = new Enemy(enemies.basic);
+    enemy2 = new Enemy(enemies.basic);
+
+    board.add(enemy);
+    board.add(enemy2);
+    spyOn(board,'remove');
     
+    enemy.x = Game.width +1;
+    enemy2.y = Game.height+ 1;
+    enemy.step(0);
+    
+    enemy2.step(0);
+    expect(board.remove).toHaveBeenCalled();
+    expect(board.remove.calls.length).toBe(2);
   });
 
 });
